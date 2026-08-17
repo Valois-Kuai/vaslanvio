ae0c0fa41709cef6e9ffe9bf4434aa3d671ebe5b
Valois-Kuai <2899639236@qq.com>
2026-08-18 05:06:44 +0800

    第十次

--- JS/doc.js
+++ JS/doc.js
@@ -1,5 +1,6 @@
 const KanKin0 = document.getElementById("d0");
 const KanKin1 = document.getElementById("d1");
+const KanKin2 = document.getElementById("d2");
 const HuaTioMeVion = document.getElementById("huatiomevion");
 
 function DownLoad(sus){
@@ -13,6 +14,10 @@ function DownLoad(sus){
         a.href="./file/人类语字体与码位表.rar";
         a.download="人类语字体与码位表.rar";
     }
+    if(sus==2){
+        a.href="./file/人类语造字法.xlsx";
+        a.download="人类语造字法.xlsx";
+    }
     
     document.body.appendChild(a);
     a.click();
@@ -21,6 +26,7 @@ function DownLoad(sus){
 
 KanKin0.onclick = () => DownLoad(0); //这个所谓叫闭包的写法真的看不懂
 KanKin1.onclick = () => DownLoad(1);
+KanKin2.onclick = () => DownLoad(2);
 
 setTimeout(()=>{
     HuaTioMeVion.style.zIndex=0;
