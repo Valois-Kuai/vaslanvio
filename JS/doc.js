806d412cc9d6ca730c4a5043b8a650329ea66b93
Valois-Kuai <2899639236@qq.com>
2026-08-26 11:56:33 +0800

    第十七次

--- JS/doc.js
+++ JS/doc.js
@@ -1,6 +1,8 @@
 const KanKin0 = document.getElementById("d0");
 const KanKin1 = document.getElementById("d1");
 const KanKin2 = document.getElementById("d2");
+const KanKin3 = document.getElementById("d3");
+const KanKin4 = document.getElementById("d4");
 const HuaTioMeVion = document.getElementById("huatiomevion");
 
 function DownLoad(sus){
@@ -18,7 +20,15 @@ function DownLoad(sus){
         a.href="./file/人类语造字法.xlsx";
         a.download="人类语造字法.xlsx";
     }
+    if(sus==3){
+        a.href="./file/人类语非强制语法规范合集.pdf";
+        a.download="人类语非强制语法规范合集.pdf";
+    }
     
+    if(sus==4){
+        a.href="./file/人类语万词表.xlsx";
+        a.download="人类语万词表.xlsx";
+    }
     document.body.appendChild(a);
     a.click();
     document.body.removeChild(a);
@@ -27,6 +37,8 @@ function DownLoad(sus){
 KanKin0.onclick = () => DownLoad(0); //这个所谓叫闭包的写法真的看不懂
 KanKin1.onclick = () => DownLoad(1);
 KanKin2.onclick = () => DownLoad(2);
+KanKin3.onclick = () => DownLoad(3);
+KanKin4.onclick = () => DownLoad(4);
 
 setTimeout(()=>{
     HuaTioMeVion.style.zIndex=0;
